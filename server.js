07e73d868dbbb5446bf1a4abef70b4ae57ffd4ec
Pasante TICS <pasante3_tics@cresio.com>
2026-03-04 15:39:10 -0500

    fix cors

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
